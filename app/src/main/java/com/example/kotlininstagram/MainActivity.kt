ef4be3ba9d2e13c73ad7cd8a30f43feee13bc7c3
Eda Dursun <edadrsn929@gmail.com>
2025-05-07 22:59:00 +0300

    sign up functions created

--- app/src/main/java/com/example/kotlininstagram/MainActivity.kt
+++ app/src/main/java/com/example/kotlininstagram/MainActivity.kt
@@ -1,33 +1,66 @@
 package com.example.kotlininstagram
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlininstagram.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.auth.ktx.auth
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Authu initialize etmenin 2 yolu var
+        //1-auth=FirebaseAuth.getInstance()
+        //2-auth= Firebase.auth
 
+        auth = Firebase.auth
 
 
     }
 
-    fun signInClicked(view: View){
+    fun signInClicked(view: View) {
 
     }
 
-    fun signUpClicked(view:View){
-
+    fun signUpClicked(view: View) {
+        //KAYDOL
+        //SIGN UP NEW USER:Yeni kullanıcı oluşturma
+        val email = binding.emailText.text.toString()
+        val password = binding.passwordText.text.toString()
+        if (email.equals("") || password.equals("")) {
+            Toast.makeText(this@MainActivity, "Enter email and password", Toast.LENGTH_SHORT).show()
+        } else {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener {   //Sadece başarılıysa çağırılır
+                    val intent = Intent(this@MainActivity, FeedActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                .addOnFailureListener {    //Sadece hata varsa çağırılır
+                    Toast.makeText(
+                        this@MainActivity,
+                        it.localizedMessage,  //Hata mesajını kullanıcının anlayacağı dilde yazdır
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
     }
+
+
 }
