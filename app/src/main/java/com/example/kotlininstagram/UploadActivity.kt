b8bc5157184386712a74d10453aca0662a26fcaa
Eda Dursun <edadrsn929@gmail.com>
2025-05-08 14:45:27 +0300

    the functions required for camera access permission have been created

--- app/src/main/java/com/example/kotlininstagram/UploadActivity.kt
+++ app/src/main/java/com/example/kotlininstagram/UploadActivity.kt
@@ -1,33 +1,169 @@
 package com.example.kotlininstagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.sqlite.SQLiteDatabase
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kotlininstagram.databinding.ActivityUploadBinding
+import com.google.android.material.snackbar.Snackbar
 
 class UploadActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityUploadBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    var selectedBitmap: Bitmap? = null //selectedPicture
+    private lateinit var database: SQLiteDatabase
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding=ActivityUploadBinding.inflate(layoutInflater)
+        binding = ActivityUploadBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-    }
-
-
-    fun upload(view: View){
+        //database=this.openOrCreateDatabase("Images", MODE_PRIVATE,null)
+        registerLauncher()
 
     }
 
 
-    fun selectImage(view:View){
+    fun upload(view: View) {
 
     }
+
+
+    //Resim seçme ve kamera izni
+    fun selectImage(view: View) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            //Android 33+ -> READ MEDIA IMAGES
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                //İzin isteme mantığını kullanıcıya açıkla
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
+                    Snackbar.make(view, "Permission Needed For Gallery", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Give Permission", View.OnClickListener {
+                            //İzin iste
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+                } else {
+                    //İzin iste
+                    permissionLauncher.launch((Manifest.permission.READ_MEDIA_IMAGES))
+                }
+            }
+            //İzin verildi
+            else {
+                //Galeriye git
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+            }
+        } else {
+            //Android 32 - -> READ_EXTERNAL_STORAGE
+            //Daha önceden izin verilmiş mi kontrol et
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {//İzin verilmedi
+                //İzin isteme mantığını kullanıcıya açıkla
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
+                    Snackbar.make(view, "Permission Needed For Gallery", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Give Permission", View.OnClickListener {
+                            //İzin iste
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }).show()
+                } else {
+                    //İzin iste
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
+            //İzin verildi
+            else {
+                //Galeriye git
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGallery)
+
+
+            }
+        }
+    }
+
+
+    //RegisterLauncher:Intent sonucu almak
+    //PermissionLauncher:İzin istemek
+    private fun registerLauncher() {
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        val imageData = intentFromResult.data
+                        if (imageData != null) {
+                            try {
+                                if (Build.VERSION.SDK_INT >= 28) {
+                                    val source = ImageDecoder.createSource(
+                                        this@UploadActivity.contentResolver,
+                                        imageData
+                                    )
+                                    selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                    binding.imageView3.setImageBitmap(selectedBitmap)
+                                } else {
+                                    selectedBitmap = MediaStore.Images.Media.getBitmap(
+                                        contentResolver,
+                                        imageData
+                                    )
+                                    binding.imageView3.setImageBitmap(selectedBitmap)
+                                }
+                            } catch (e: Exception) {
+                                e.printStackTrace()
+                            }
+                        }
+                    }
+                }
+            }
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                } else {
+                    Toast.makeText(this@UploadActivity, "Permission needed!", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+            }
+    }
+
 
 }
