b767405a1f0d595e6aa2dea2b8945f674b0957f9
Eda Dursun <edadrsn929@gmail.com>
2025-05-08 17:53:11 +0300

    Feed Recycler Adapter created

--- app/src/main/java/com/example/kotlininstagram/adapter/FeedRecyclerAdapter.kt
+++ app/src/main/java/com/example/kotlininstagram/adapter/FeedRecyclerAdapter.kt
@@ -1,4 +1,28 @@
 package com.example.kotlininstagram.adapter
 
-class FeedRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlininstagram.databinding.RecyclerRowBinding
+import com.example.kotlininstagram.model.Post
+
+class FeedRecyclerAdapter(val postList:ArrayList<Post>): RecyclerView.Adapter<FeedRecyclerAdapter.PostHolder>(){
+
+    class PostHolder(val binding:RecyclerRowBinding):RecyclerView.ViewHolder(binding.root){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        val binding=RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return PostHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+      return postList.size
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+        holder.binding.recyclerEmailText.text=postList.get(position).email
+        holder.binding.recyclerComment.text=postList.get(position).comment
+    }
 }
